f9ca607ae3b42b0bfb47e7168a2316a740f702c3
Tempo <alexleg33@outlook.fr>
2020-01-31 21:04:25 +0100

    feat: camera optimized

--- lib/dragon/src/ecs/systems/dg_sys_render.c
+++ lib/dragon/src/ecs/systems/dg_sys_render.c
@@ -9,6 +9,23 @@
 #include "libdragon.h"
 #include "epitech_tools.h"
 
+static int is_in_screen(sfSprite *sprite, sfVector2f camera, dg_window_t *w)
+{
+    sfFloatRect rect = {0};
+    sfVector2u window = {0};
+    int stat = 1;
+
+    if (!sprite || !w)
+        return 0;
+    rect = sfSprite_getGlobalBounds(sprite);
+    window = sfRenderWindow_getSize(w->window);
+    stat = (rect.left + rect.width - camera.x < 0) ? 0 : stat;
+    stat = (rect.top + rect.height - camera.y < 0) ? 0 : stat;
+    stat = (rect.left - camera.x > window.x) ? 0 : stat;
+    stat = (rect.top - camera.y > window.y) ? 0 : stat;
+    return stat;
+}
+
 static void display_sprite(sfVector2f camera, dg_entity_t *entity,
     dg_window_t *w)
 {
@@ -17,9 +34,11 @@ static void display_sprite(sfVector2f camera, dg_entity_t *entity,
 
     if (dg_system_require(entity, 1, "sprite")) {
             sprite = (sfSprite *)(dg_entity_get_component(entity, "sprite"));
-            sfSprite_move(sprite, reverse);
-            sfRenderWindow_drawSprite(w->window, sprite, NULL);
-            sfSprite_move(sprite, camera);
+            if (is_in_screen(sprite, camera, w)) {
+                sfSprite_move(sprite, reverse);
+                sfRenderWindow_drawSprite(w->window, sprite, NULL);
+                sfSprite_move(sprite, camera);
+            }
         }
 }
 
