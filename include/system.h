3dde5048a757a6cfd29280f615845a7c57b162d1
Huntears <alexandre.flion@epitech.eu>
2020-02-01 09:10:23 +0100

    Add dynamic camera

--- include/system.h
+++ include/system.h
@@ -5,7 +5,7 @@
 
 void sys_set_pos_sprite(dg_entity_t *entity, dg_window_t *w, dg_array_t **entities, sfTime dt);
 void sys_gravity(dg_entity_t *, dg_window_t *, dg_array_t **, sfTime);
-void system_player_control(dg_entity_t *entity, dg_window_t *w,
-    dg_array_t **entities, sfTime dt);
+void system_player_control(dg_entity_t *entity, dg_window_t *w, dg_array_t **entities, sfTime dt);
+void sys_camera_follow_player(dg_entity_t *entity, dg_window_t *w, dg_array_t **entities, sfTime dt);
 
 #endif /* !SYSTEM_H_ */
--- src/game_src/entities/ent_player.c
+++ src/game_src/entities/ent_player.c
@@ -41,11 +41,12 @@ static dg_animator_t *set_player_animator(void)
 dg_entity_t *ent_player(int x, int y)
 {
     dg_entity_t *entity = dg_entity_create("player");
+    sfVector2f scale = {5, 5};
 
-    dg_entity_add_component(entity, cpt_box_collider(0, 0, 32, 32));
+    dg_entity_add_component(entity, cpt_box_collider(0, 0, 32 * 5, 32 * 5));
     dg_entity_add_component(entity, cpt_rigid_body(1, 1));
     dg_entity_add_component(entity, dg_cpt_pos(x, y));
-    dg_entity_add_component(entity, dg_cpt_sprite(NULL));
+    dg_entity_add_component(entity, dg_cpt_sprite(&scale));
     dg_entity_add_component(entity, dg_cpt_animator(&set_player_animator));
     return entity;
 }
