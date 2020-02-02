fe6a97c7644d51a65ffeb465b651a1ecfc62030b
Huntears <alexandre.flion@epitech.eu>
2020-02-02 06:23:49 +0100

    Fix the basic crafting system

--- src/game_src/system/sys_crafting.c
+++ src/game_src/system/sys_crafting.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include "libdragon.h"
 #include "inventory.h"
 #include "string.h"
+#include "entity.h"
 
 void sys_crafting(dg_entity_t *entity, dg_window_t *w, dg_array_t **entities, sfTime dt)
 {
@@ -10,10 +12,10 @@ void sys_crafting(dg_entity_t *entity, dg_window_t *w, dg_array_t **entities, sf
     (void)w;
     (void)dt;
     (void)entity;
-    if (!player)
+    if (!player || !inv || !inv->left || !inv->right)
         return;
     if (((!strcmp(inv->left->name, "broken_cog_left") && !strcmp(inv->right->name, "broken_cog_right")) || (!strcmp(inv->left->name, "broken_cog_right") && !strcmp(inv->right->name, "broken_cog_left"))) && sfMouse_isButtonPressed(sfMouseMiddle)) {
-        inv->left = 0;
+        inv->left = ent_object("cog", dg_spritesheet_create("./res/cog.png", 32, 32), (sfVector2f) {0, 0}, (sfVector2f) {64, 64});
         inv->right = 0;
         return;
     }
