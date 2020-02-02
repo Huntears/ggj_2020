fc303f8ccd44e950080442b349af960debb0f7ae
alexandre.descoins <alexandre.descoins@epitech.eu>
2020-02-02 15:18:45 +0100

    [ADD]:  basic menu

--- src/game_src/scene/scene_game.c
+++ src/game_src/scene/scene_game.c
@@ -6,6 +6,14 @@
 dg_scene_t *scene_game_create(void)
 {
     dg_scene_t *scene_game = dg_scene_create();
+    int **map = read_csv_from_file("text.tmp");
+
+    dg_scene_add_ent(scene_game, ent_player(100, 100));
+
+    for (int i = 0; map[i][0] != -2; i++)
+        for (int z = 0; map[i][z] != -1; z++)
+            if (map[i][z])
+                dg_scene_add_ent(scene_game, ent_decor(z * 32 * 5, i * 32 * 5, map[i][z] - 1));
 
     dg_scene_add_ent(scene_game, dg_ent_camera(0, 0));
     dg_scene_add_sys(scene_game, dg_system_create(&dg_sys_animator));
